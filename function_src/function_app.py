68d6dd21eeb6170cfb6859884fc132ffd4f9f9a3
isa31188 <isa31188@gmail.com>
2025-02-05 14:09:04 +0000

    Replacing function app code and requirements.

---
+++ function_src/function_app.py
@@ -0,0 +1,16 @@
+import logging
+
+import azure.functions as func
+import azurefunctions.extensions.bindings.blob as blob
+
+# Just for testing correct application of requirements.txt
+from seaborn import load_dataset
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+@app.blob_trigger(arg_name="client", path="source/{name}.jpg", connection="AzureWebJobsStorage")
+def blob_trigger(client: blob.BlobClient):
+    logging.info(
+        f"Python blob trigger function processed blob \n"
+        f"Properties: {client.get_blob_properties()}\n"
+        f"Blob content head: {client.download_blob().read(size=1)}"
+    )
