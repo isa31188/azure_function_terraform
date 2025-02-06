73d14cbd56741a6dc1a89134fae410173bd989cf
isa31188 <isa31188@gmail.com>
2025-02-06 11:06:54 +0000

    Correcting typo in python script.

--- functions/helloworld/__init__.py
+++ functions/helloworld/__init__.py
@@ -2,7 +2,7 @@ import logging
 import azure.functions as func
 
 
-def main(myblob: func.InputStream, inputblob: bytes, outputblob: func.Out[bytes]):):
+def main(myblob: func.InputStream, inputblob: bytes, outputblob: func.Out[bytes]):
     logging.info('Python Blob trigger function processed %s', myblob.name)
     logging.info(f'Python Queue trigger function processed {len(inputblob)} bytes')
     outputblob.set(inputblob)
