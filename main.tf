1c04e75318a76b7ffc4044f437065950aa5c6423
isa31188 <isa31188@gmail.com>
2025-02-06 14:20:51 +0000

    Upgrade the function app tf resource (handle warning for deprecated resource).

--- main.tf
+++ main.tf
@@ -7,10 +7,6 @@ resource "azurerm_application_insights" "app-insights" {
   tags                = var.tags
 }
 
-#data "azurerm_resource_group" "rg" {
-#  name     = "niels-zeilemaker-sandbox"
-#}
-
 data "azurerm_resource_group" "resource_group" {
   name = "oym-deploy-sand-rg"
 }
@@ -26,26 +22,13 @@ data "archive_file" "function" {
   output_path = "${path.module}/functions.zip"
 }
 
-#resource "azurerm_storage_account" "storage_account_function" {
-#  name                     = "st${var.owner}${var.project_name}fn"
-#  resource_group_name      = data.azurerm_resource_group.rg.name
-#  location                 = var.location
-#  account_tier             = "standard"
-#  account_replication_type = "LRS"
-#  account_kind             = "StorageV2"
-#  is_hns_enabled           = true
-#  tags                     = var.tags
-#}
-
 resource "azurerm_storage_container" "storage_container_function" {
-  name = "function-releases"
-  #storage_account_name = data.azurerm_storage_account.storage_account.name
+  name               = "function-releases"
   storage_account_id = data.azurerm_storage_account.storage_account.id
 }
 
 resource "azurerm_storage_container" "source" {
-  name = "source"
-  #storage_account_name = data.azurerm_storage_account.storage_account.name
+  name               = "source"
   storage_account_id = data.azurerm_storage_account.storage_account.id
 }
 
@@ -58,36 +41,6 @@ resource "azurerm_storage_blob" "storage_blob_function" {
   source                 = "${path.module}/functions.zip"
 }
 
-#resource "azurerm_eventhub_namespace" "main" {
-#  location            = var.location
-#  name                = "eh-${var.owner}-${var.project_name}-datadog"
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#  sku                 = "Standard"
-#  capacity            = 1
-#  tags                = var.tags
-#}
-#resource "azurerm_eventhub" "main" {
-#  namespace_name      = azurerm_eventhub_namespace.main.name
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#  name                = "events"
-#  message_retention   = 1
-#  partition_count     = 2
-#}
-#resource "azurerm_eventhub_consumer_group" "fa" {
-#  eventhub_name       = azurerm_eventhub.main.name
-#  name                = "fa"
-#  namespace_name      = azurerm_eventhub_namespace.main.name
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#}
-
-#resource "azurerm_application_insights" "app-insights" {
-#  application_type    = "web"
-#  location            = var.location
-#  name                = "app-${var.owner}-${var.project_name}"
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#  tags                = var.tags
-#}
-
 resource "azurerm_service_plan" "main" {
   name                = "${var.project_name}-asp"
   location            = var.location
@@ -97,9 +50,9 @@ resource "azurerm_service_plan" "main" {
   sku_name            = "Y1"
 }
 
-resource "azurerm_function_app" "function-app" {
+resource "azurerm_linux_function_app" "function-app" {
   resource_group_name = data.azurerm_resource_group.resource_group.name
-  app_service_plan_id = azurerm_service_plan.main.id
+  service_plan_id     = azurerm_service_plan.main.id
   location            = var.location
 
   storage_account_name       = data.azurerm_storage_account.storage_account.name
@@ -107,13 +60,15 @@ resource "azurerm_function_app" "function-app" {
   name                       = "${var.project_name}-fa"
   tags                       = var.tags
 
-  enable_builtin_logging = false
-  os_type                = "linux"
-  version                = "~4"
+  builtin_logging_enabled = false
+
+  functions_extension_version = "~4"
 
   site_config {
-    linux_fx_version          = "PYTHON|3.9"
-    use_32_bit_worker_process = false
+    use_32_bit_worker = false
+    application_stack {
+      python_version = "3.9"
+    }
   }
 
   identity {
@@ -122,9 +77,7 @@ resource "azurerm_function_app" "function-app" {
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "python"
-    #"EventHub_AccessKey"         = azurerm_eventhub_namespace.main.default_primary_connection_string
     "WEBSITE_RUN_FROM_PACKAGE" = azurerm_storage_blob.storage_blob_function.url
-    #"APPINSIGHTS_INSTRUMENTATIONKEY"           = azurerm_application_insights.app-insights.instrumentation_key
     "MyStorageConnectionAppSetting" : data.azurerm_storage_account.storage_account.primary_connection_string
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app-insights.instrumentation_key
   }
@@ -133,6 +86,6 @@ resource "azurerm_function_app" "function-app" {
 resource "azurerm_role_assignment" "role_assignment_storage" {
   scope                            = data.azurerm_storage_account.storage_account.id
   role_definition_name             = "Storage Blob Data Contributor"
-  principal_id                     = azurerm_function_app.function-app.identity.0.principal_id
+  principal_id                     = azurerm_linux_function_app.function-app.identity.0.principal_id
   skip_service_principal_aad_check = true
 }
