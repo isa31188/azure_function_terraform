904a47431c2c2d548b11cfc0f7e32477bc335581
isa31188 <isa31188@gmail.com>
2025-02-07 11:59:46 +0000

    Improvement of remote_build automation.

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
-
 # For function and plan names. To avoid issues with automatically 
 # created artifacts, when deploying always using the same names.
 resource "random_string" "random" {
-  length           = 4
-  lower            = true
-  min_lower        = 4
+  length    = 4
+  lower     = true
+  min_lower = 4
 }
-
 resource "azurerm_application_insights" "app-insights" {
   application_type    = "web"
   location            = var.location
@@ -14,16 +12,13 @@ resource "azurerm_application_insights" "app-insights" {
   resource_group_name = data.azurerm_resource_group.resource_group.name
   tags                = var.tags
 }
-
 data "azurerm_resource_group" "resource_group" {
   name = "oym-deploy-sand-rg"
 }
-
 data "azurerm_storage_account" "storage_account" {
   name                = "oymdeploysandiacdl01"
   resource_group_name = data.azurerm_resource_group.resource_group.name
 }
-
 resource "null_resource" "pip" {
   triggers = {
     requirements_md5 = "${filemd5("${path.module}/functions/requirements.txt")}"
@@ -33,24 +28,20 @@ resource "null_resource" "pip" {
     working_dir = "${path.module}/functions"
   }
 }
-
 data "archive_file" "function" {
   type        = "zip"
   source_dir  = "${path.module}/functions"
   output_path = "${path.module}/functions.zip"
   depends_on  = [null_resource.pip]
 }
-
 resource "azurerm_storage_container" "storage_container_function" {
   name               = "function-releases"
   storage_account_id = data.azurerm_storage_account.storage_account.id
 }
-
 resource "azurerm_storage_container" "source" {
   name               = "source"
   storage_account_id = data.azurerm_storage_account.storage_account.id
 }
-
 resource "azurerm_storage_blob" "storage_blob_function" {
   name                   = "functions-${substr(data.archive_file.function.output_md5, 0, 6)}.zip"
   storage_account_name   = data.azurerm_storage_account.storage_account.name
@@ -59,7 +50,6 @@ resource "azurerm_storage_blob" "storage_blob_function" {
   content_md5            = data.archive_file.function.output_md5
   source                 = "${path.module}/functions.zip"
 }
-
 resource "azurerm_service_plan" "main" {
   name                = "${var.project_name}-${random_string.random.id}-asp"
   location            = var.location
@@ -68,40 +58,51 @@ resource "azurerm_service_plan" "main" {
   tags                = var.tags
   sku_name            = "Y1"
 }
-
 resource "azurerm_linux_function_app" "function-app" {
   resource_group_name = data.azurerm_resource_group.resource_group.name
   service_plan_id     = azurerm_service_plan.main.id
   location            = var.location
-
   storage_account_name       = data.azurerm_storage_account.storage_account.name
   storage_account_access_key = data.azurerm_storage_account.storage_account.primary_access_key
   name                       = "${var.project_name}-${random_string.random.id}-fa"
   tags                       = var.tags
-
   builtin_logging_enabled = false
-
   functions_extension_version = "~4"
-
   site_config {
     use_32_bit_worker = false
     application_stack {
       python_version = "3.9"
     }
+    application_insights_key = azurerm_application_insights.app-insights.instrumentation_key
   }
-
   identity {
     type = "SystemAssigned"
   }
-
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "WEBSITE_RUN_FROM_PACKAGE" = azurerm_storage_blob.storage_blob_function.url
-    "MyStorageConnectionAppSetting" : data.azurerm_storage_account.storage_account.primary_connection_string
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app-insights.instrumentation_key
+    "FUNCTIONS_WORKER_RUNTIME"       = "python"
+    "WEBSITE_RUN_FROM_PACKAGE"       = var.remote_build ? null : azurerm_storage_blob.storage_blob_function.url
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = var.remote_build ? true : null
+    "MyStorageConnectionAppSetting"  = data.azurerm_storage_account.storage_account.primary_connection_string
+    # This is actually automatically set by terraform by defining application_insights_key in the site_config block.
+    #"APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app-insights.instrumentation_key
   }
 }
-
+resource "null_resource" "func_deploy" {
+  provisioner "local-exec" {
+    command = (
+      var.remote_build ?
+      <<-EOT
+      az functionapp deployment source config-zip \
+      --resource-group ${data.azurerm_resource_group.resource_group.name} \
+      --name ${azurerm_linux_function_app.function-app.name} \
+      --src functions.zip \
+      --build-remote true
+      EOT
+    : "echo 'No further commands needed.'") # run command only if remote_build
+    working_dir = path.module
+  }
+  depends_on = [azurerm_linux_function_app.function-app]
+}
 resource "azurerm_role_assignment" "role_assignment_storage" {
   scope                            = data.azurerm_storage_account.storage_account.id
   role_definition_name             = "Storage Blob Data Contributor"
